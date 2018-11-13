323b3c6ec0a5a2350b66b011dcf35d4bd5396b14
Kees Hiemstra <chi@xs4all.nl>
2018-11-13 23:14:11 +0100

    My culture preference

---
+++ CHi culture preference.ps1
@@ -0,0 +1,21 @@
+﻿#
+# Adjusted Culture
+#
+
+$ThisCulture = Get-Culture
+$ThisCulture.TextInfo.ListSeparator = ";"
+$ThisCulture.NumberFormat.CurrencyDecimalSeparator = "."
+$ThisCulture.NumberFormat.CurrencyGroupSeparator = ","
+$ThisCulture.NumberFormat.NumberDecimalSeparator = "."
+$ThisCulture.NumberFormat.NumberGroupSeparator = ","
+$ThisCulture.NumberFormat.PercentDecimalSeparator = "."
+$ThisCulture.NumberFormat.PercentGroupSeparator = ","
+$ThisCulture.DateTimeFormat.DateSeparator = "-"
+$ThisCulture.DateTimeFormat.FirstDayOfWeek = [System.DayOfWeek]::Monday
+$ThisCulture.DateTimeFormat.CalendarWeekRule = [System.Globalization.CalendarWeekRule]::FirstFourDayWeek
+$ThisCulture.DateTimeFormat.LongTimePattern = "HH:mm:ss"
+$ThisCulture.DateTimeFormat.ShortDatePattern = "yyyy-MM-dd"
+$ThisCulture.DateTimeFormat.ShortTimePattern = "HH:mm"
+$ThisCulture.DateTimeFormat.TimeSeparator = ":"
+
+$ThisCulture | Set-Culture
