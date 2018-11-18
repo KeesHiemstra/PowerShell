c2c24ac0f018c78db3f36e54628dd4d048063c36
Kees Hiemstra <chi@xs4all.nl>
2018-11-18 13:26:05 +0100

    Initial Export of Journal

---
+++ ExportJournal.ps1
@@ -0,0 +1,24 @@
+﻿#
+# Export the Journal table to cvs file
+#
+
+if (-not (Test-Path -Path "$($env:OneDrive)\Data"))
+{
+    Write-Error "The location doesn't exist"
+    break
+}
+
+[string]$ComputerName = $env:COMPUTERNAME
+[string]$SQL = "SELECT [LogID], [DTStart], [Event], [Message] FROM Journal ORDER BY [DTStart], [LogID]"
+[string]$CsvFile = "$($env:OneDrive)\Data\Journal.csv"
+
+Push-Location
+
+Import-Module SQLPS -DisableNameChecking
+
+Pop-Location
+
+$Journal = Invoke-Sqlcmd -Query $SQL -ServerInstance $ComputerName -database Joost 
+
+$Journal |
+    Export-Csv -Path $CsvFile -NoTypeInformation -UseCulture
