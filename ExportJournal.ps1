df21185052ff11ac14dab00e4a369fbcc96621e9
Kees Hiemstra <chi@xs4all.nl>
2018-11-18 13:28:47 +0100

    Added culture to Export of Journal

--- ExportJournal.ps1
+++ ExportJournal.ps1
@@ -8,6 +8,25 @@ if (-not (Test-Path -Path "$($env:OneDrive)\Data"))
     break
 }
 
+# Set culture for proper export for csv
+$ThisCulture = Get-Culture
+$ThisCulture.TextInfo.ListSeparator = ";"
+$ThisCulture.NumberFormat.CurrencyDecimalSeparator = "."
+$ThisCulture.NumberFormat.CurrencyGroupSeparator = ","
+$ThisCulture.NumberFormat.NumberDecimalSeparator = "."
+$ThisCulture.NumberFormat.NumberGroupSeparator = ","
+$ThisCulture.NumberFormat.PercentDecimalSeparator = "."
+$ThisCulture.NumberFormat.PercentGroupSeparator = ","
+$ThisCulture.DateTimeFormat.DateSeparator = "-"
+$ThisCulture.DateTimeFormat.FirstDayOfWeek = [System.DayOfWeek]::Monday
+$ThisCulture.DateTimeFormat.CalendarWeekRule = [System.Globalization.CalendarWeekRule]::FirstFourDayWeek
+$ThisCulture.DateTimeFormat.LongTimePattern = "HH:mm:ss"
+$ThisCulture.DateTimeFormat.ShortDatePattern = "yyyy-MM-dd"
+$ThisCulture.DateTimeFormat.ShortTimePattern = "HH:mm"
+$ThisCulture.DateTimeFormat.TimeSeparator = ":"
+
+$ThisCulture | Set-Culture
+
 [string]$ComputerName = $env:COMPUTERNAME
 [string]$SQL = "SELECT [LogID], [DTStart], [Event], [Message] FROM Journal ORDER BY [DTStart], [LogID]"
 [string]$CsvFile = "$($env:OneDrive)\Data\Journal.csv"
